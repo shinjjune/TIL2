143b11fc78f2f4efef9911f3330646a4ed455716
unknown <sjh8236@naver.com>
2020-08-15 22:46:10 +0900

    room admin

--- airabnb-clone/rooms/admin.py
+++ airabnb-clone/rooms/admin.py
@@ -15,7 +15,60 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    fieldsets = (
+        (
+            "Basic Info",
+            {"fields": ("name", "description", "country", "address", "price")},
+        ),
+        ("Times", {"fields": ("check_in", "check_out", "instant_book")}),
+        ("Spaces", {"fields": ("guests", "beds", "bedrooms", "baths")}),
+        (
+            "More About the Space",
+            {
+                "classes": ("collapse",),
+                "fields": ("amenities", "facilities", "house_rule"),
+            },
+        ),
+        ("Last Details", {"fields": ("host",)}),
+    )
+
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",
+        "count_amenities",
+    )
+
+    ordering = ("name", "price", "bedrooms")
+
+    list_filter = (
+        "instant_book",
+        "host__superhost",
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rule",
+        "city",
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+
+    filter_horizontal = ("amenities", "facilities", "house_rule")
+
+    def count_amenities(self, obj):
+        print(obj.amenities.all())
+        return "potato"
+
+    count_amenities.short_description = "hello sexy"
 
 
 @admin.register(models.Photo)
