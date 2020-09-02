3012f85c90afe7a40ac3927fa4f9194de311999c
unknown <sjh8236@naver.com>
2020-09-02 23:04:48 +0900

    debug = false

--- airabnb-clone/rooms/views.py
+++ airabnb-clone/rooms/views.py
@@ -1,4 +1,6 @@
 from django.utils import timezone
+from django.http import Http404
+from django.shortcuts import render
 from django.views.generic import ListView
 from . import models
 
@@ -13,8 +15,16 @@ class HomeView(ListView):
     ordering = "created"
     context_object_name = "rooms"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        now = timezone.now()
-        context["now"] = now
-        return context
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     now = timezone.now()
+    #     context["now"] = now
+    #     return context
+
+
+def room_detail(request, pk):
+    try:
+        room = models.Room.objects.get(pk=pk)
+        return render(request, "rooms/detail.html", {"room": room})
+    except models.Room.DoesNotExist:
+        raise Http404()
