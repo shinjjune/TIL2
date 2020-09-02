ad3b224e5d6d09f9c41ac10387da83636d54ff41
unknown <sjh8236@naver.com>
2020-09-02 23:11:48 +0900

    class&func

--- airabnb-clone/config/settings.py
+++ airabnb-clone/config/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "c1&)jple%+r(4*k^feuy@r4u)j!z5$^e*yq9t80hcz38x-08hm"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = "*"
+ALLOWED_HOSTS = []
 
 
 # Application definition
