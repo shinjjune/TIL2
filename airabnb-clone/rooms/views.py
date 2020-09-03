876cff9a57239b09b7bba27bfc52a0cf397bdd0a
unknown <sjh8236@naver.com>
2020-09-03 22:10:42 +0900

    Search View

--- airabnb-clone/rooms/views.py
+++ airabnb-clone/rooms/views.py
@@ -1,5 +1,6 @@
-from django.utils import timezone
-from django.http import Http404
+# from django.utils import timezone
+# from django.http import Http404
+from django_countries import countries
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from . import models
@@ -35,3 +36,14 @@ class RoomDetail(DetailView):
     """ RoomDetail Definition """
 
     model = models.Room
+
+
+def search(request):
+    city = request.GET.get("city", "Anywhere")
+    city = str.capitalize(city)
+    room_types = models.RoomType.objects.all()
+    return render(
+        request,
+        "rooms/search.html",
+        {"city": city, "countries": countries, "room_types": room_types},
+    )
