a98c4e0c2941a01ac713e407d9261fd45e3b4b73
unknown <sjh8236@naver.com>
2020-09-05 22:29:49 +0900

    olleh

--- airabnb-clone/rooms/forms.py
+++ airabnb-clone/rooms/forms.py
@@ -1,9 +1,31 @@
 from django import forms
 from . import models
+from django_countries.fields import CountryField
 
 
 class SearchForm(forms.Form):
 
     city = forms.CharField(initial="Anywhere")
+    country = CountryField(default="KR").formfield()
+    room_type = forms.ModelChoiceField(
+        required=False, empty_label="Any kind", queryset=models.RoomType.objects.all()
+    )
     price = forms.IntegerField(required=False)
-    room_type = forms.ModelChoiceField(queryset=models.RoomType.objects.all())
+    guests = forms.IntegerField(required=False)
+    bedrooms = forms.IntegerField(required=False)
+    beds = forms.IntegerField(required=False)
+    baths = forms.IntegerField(required=False)
+    instant_book = forms.BooleanField(required=False)
+    superhost = forms.BooleanField(required=False)
+
+    amenities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Amenity.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    facilities = forms.ModelMultipleChoiceField(
+        required=False,
+        queryset=models.Facility.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
